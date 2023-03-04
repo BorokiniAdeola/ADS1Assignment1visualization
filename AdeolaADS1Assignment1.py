00a37cbb81a1f5f52ee5cd38c99d0ff995dc8cf7
BorokiniAdeola <126989015+BorokiniAdeola@users.noreply.github.com>
2023-03-04 21:43:45 +0000

    Add files via upload

--- AdeolaADS1Assignment1.py
+++ AdeolaADS1Assignment1.py
@@ -3,9 +3,13 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
+# Reading the dataset in to a DataFrame
 gdp_scan_country = pd.read_csv('GDP_data_2015_to_2019_Finland_Norway_Sweden.csv', index_col=0)
 
+# Creating the list of year to be plotted
 gdp_year = ['2015', '2016', '2017', '2018', '2019']
+
+# This function is used to plot a line graph of multiple lines
 
 def plot_gdp_lines(gdp_scan_country, x_col, y_cols, title, x_label, y_label):
     """
@@ -31,7 +35,7 @@ def plot_gdp_lines(gdp_scan_country, x_col, y_cols, title, x_label, y_label):
 
 plot_gdp_lines(gdp_scan_country, 'year', list(gdp_scan_country.columns), 'GDP of Finland Norway & Sweden (2015-2019)', 'Year', 'GDP (in billions of USD)')
 
-
+# This function is used to plot a stacked bar chart
 def plot_gdp_stacked_bar(gdp_scan_country):
     """
     Plots a stacked bar chart of GDP for Nordic countries in the selected year.
@@ -51,7 +55,7 @@ def plot_gdp_stacked_bar(gdp_scan_country):
 
 plot_gdp_stacked_bar(gdp_scan_country)
 
-
+# This function is used to plot a pie chart
 def plot_pie_chart(year, countries):
     """
     Plots a pie chart of the GDPs of different countries for a particular year.
@@ -63,17 +67,15 @@ def plot_pie_chart(year, countries):
 
     Returns:
     None
-    """
-    
+    """   
     gdp_scan = gdp_scan_country.loc[2019]
     
-    
+# Plotting a pie chart of the GDP of the Scandinavian countries  
     plt.figure()
     
     plt.pie(gdp_scan, labels=gdp_scan.index, autopct='%2.1f%%')
     plt.title(f'GDPs of 3 Scandinavian Countries(Finland,Norway & Sweden)')
-    plt.axis('equal')
-    plt.legend(loc='lower left')
+    plt.legend(loc='best')
     plt.show()
 
 
